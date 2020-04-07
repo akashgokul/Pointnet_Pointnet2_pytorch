dadc760ed12e70d3c28d35081e9b9f2d163993da
akashgokul <akashgokul@berkeley.edu>
2020-04-06 22:44:51 -0700

    gathering ply and cad id into one place

--- scratch_parser.py
+++ scratch_parser.py
@@ -7,8 +7,16 @@ chair_id_dict = {}
 number_of_chair_cad_models = 236778 #from shapenet
 for root, scene_paths, filenames in os.walk(ROOTDIR):
     for scene_path in scene_paths:
-        for chair_folder in os.listdir(root + scene_path):
-            print(os.listdir(root + scene_path + "/" + chair_folder))
+        for chair_dir in os.listdir(root + scene_path):
+            chair_dir_path = root + scene_path + "/" + chair_dir
+            chair_dir_contents = os.listdir(chair_dir_path)
+            cad_id_file = open(chair_dir_path + "/id_cad.txt", "r")
+            cad_id = cad_id_file.readline()
+            pointcloud_dir = chair_dir_path + "/model_normalized.ply"
+            print(pointcloud_dir)
+            print(cad_id)
+            chair_id_dict[pointcloud_dir] = cad_id
+
             break
         break
     break
