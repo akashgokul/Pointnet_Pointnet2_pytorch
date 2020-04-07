63df3ba25348fa400da86fcce5efab55a50435ad
akashgokul <akashgokul@berkeley.edu>
2020-04-07 14:01:05 -0700

    fixing name of file

--- data_preproc.py
+++ data_preproc.py
@@ -43,6 +43,6 @@ data = pd.DataFrame.from_dict(chair_id_dict, orient='index')
 data_dir = ROOTDIR + "data.csv"
 data.to_csv(path_or_buf=data_dir)
 print("------"*10)
-print("Done! \n Data can be found at: " + data_dir)
+print("Done! \n Data can be found at: " + data_dir + "/n")
 
 
