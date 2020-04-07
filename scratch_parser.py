2be4f5bd8d1e46413523208fa6f6a5cc75229889
akashgokul <akashgokul@berkeley.edu>
2020-04-06 22:37:50 -0700

    gathering ply and cad id into one place

--- scratch_parser.py
+++ scratch_parser.py
@@ -6,8 +6,9 @@ ROOTDIR = "/global/scratch/akashgokul/mined_scannet_chairs/"
 chair_id_dict = {}
 number_of_chair_cad_models = 236778 #from shapenet
 for root, scene_paths, filenames in os.walk(ROOTDIR):
-    print(root)
-    print(scene_paths)
+    for scene_path in scene_paths:
+        print(scene_path)
+        break
     break
         #for chair_folder in os.listdir(scene_path):
 
